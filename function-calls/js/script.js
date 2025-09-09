a4c883ba9295e44609114be5f69593c807cc3c6e
Jeany <jeany.corrius@hotmail.com>
2025-09-09 14:31:56 -0400

    Added ps5 templates

---
+++ function-calls/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Tuesday sunny afternoon
+ * APpppin fellowing students
+ * 
+ * 
+ */
+
+"use strict";
+
+/**
+ * create the canvas
+*/
+function setup() {
+ createCanvas(640, 480);
+ //create a canvas
+}
+
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+ background(255, 100, 100); 
+ //pink salmon background
+}
