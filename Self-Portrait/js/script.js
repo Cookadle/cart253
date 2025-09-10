4ee8e745133079c51eeeadd33cb68595fc9d50a8
Jeany <jeany.corrius@hotmail.com>
2025-09-10 15:40:46 -0400

    starting point of self portrait ffrl

--- Self-Portrait/js/script.js
+++ Self-Portrait/js/script.js
@@ -25,13 +25,6 @@ function setup() {
 function draw() {
     // A sky
     background('#8FD6FF');
-
-    drawHill();
-    drawSun();
-    drawHen();
-    drawPig();
-    drawTent();
-
 function drawHill() {
     // The hill is covered in grass
     push();
@@ -39,50 +32,8 @@ function drawHill() {
     noStroke();
     ellipse(750, 640, 1500, 480);
     pop();
-}
-
-function drawSun() {
-    // The sun is yellow
-    push();
-    fill('#FFFD30');
-    noStroke();
-    ellipse(320, 100, 140, 140);
-    pop();
-}
-
-
-
-function drawHenBody() {
-    push();
-    fill('#FFFFFF');
-    noStroke();
-    ellipse(320, 320, 100, 60);
-    pop();
-}
-    
 function drawHenWing() {
     push();
     stroke(0);
     ellipse(320, 320, 20, 40);
     pop();
-}
-    
-function drawHenHead() {
-    push();
-    noStroke();
-    ellipse(340, 290, 45);
-    pop();
-}
-    
-function drawHenEye() {
-    push();
-    fill(0);
-    ellipse(340, 285, 5);
-    pop();
-}
-    
-function drawHenBeak() {
-    push();
-    fill('#FFA930');
-    triangle(360, 280, 365, 285, 360, 290);
-    pop()
