2d4fcefe6a6e9b4707e19407c989d5e42c36acd5
Jeany <jeany.corrius@hotmail.com>
2025-09-26 11:43:21 -0400

    Addded many push and pop and push and pop and

--- Self-Portrait/js/script.js
+++ Self-Portrait/js/script.js
@@ -20,31 +20,36 @@ function setup() {
 function draw () {
 //grey vintage background
 background ('#C7C7C7');
-drawFace();
+drawBase();
+drawEyes();
 }
-function drawFace() {
-  ellipse (540,100,300,100); //eyes
-   //my face one color (brown) with subtle countour ,neck and shoulders darker browns and some lines
-  push();
+
+function drawBase() {
+//Rotate the rectangle aka shoulders with neck and create it
+rotate(-6)
+rect(90, 770, 540, 409); // shoulders
+//Rotate the rectangle aka neck even more n create
+rotate(-5);
+rect(305,560,119,300); //neck
+fill("#dd883dff")
+noStroke();
+  
+//My face
+ push();
   fill("#b98150ff");
   noStroke();
-  ellipse ( 350,370,380,540);
+  ellipse ( 320,370,380,540);
   
-  triangle (535,403,380,715, 227,576); //576 good
-  pop ();
-  //rotate the rectangle aka shoulders with neck
-  rotate(-6)
-  rect(90, 770, 540, 409); // shoulders
-  pop(); 
+//My chiseled chin
+triangle (504,460,400,715, 190,589); //576 good
+pop ();
+  push();
   
-  // Rotate the rectangle aka neck even more
-  rotate(-5);
-  rect(305,560,119,300); //neck
-  fill("#dd883dff")
-  noStroke();
-
 }
 function drawEyes(){
-  ellipse(50, 50, 80, 44);
+push();
+ellipse (540,100,300,100); 
+fill("##f2efed")
+pop();
 }
 
