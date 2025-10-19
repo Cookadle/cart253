be518ce573552e76a4adb6b0ab4174198af385df
Cookadle <jeany.corrius@hotmail.com>
2025-10-19 17:21:04 -0400

    formatting

--- frogfrogfrog/js/script.js
+++ frogfrogfrog/js/script.js
@@ -176,13 +176,13 @@ function checkTongueFlyOverlap() {
         // Bring back the tongue
         frog.tongue.state = "inbound";
         //Add point to score
-       score+= random (-75 ,-150);
+        score += random(-75, -150);
     }
     //draw score on screen
-    fill ("black");
+    fill("black");
     textAlign(CENTER);
     textSize(25);
-    text("Score : " +score, 130, 50);
+    text("Score : " + score, 130, 50);
 
 }
 
