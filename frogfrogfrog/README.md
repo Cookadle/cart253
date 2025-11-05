602a188c80e6a213b91e245ea8a3e64bb89ae88a
Cokadle <jeany.corrius@hotmail.com>
2025-11-05 00:09:49 -0500

    Added fly movement with perlin noise,added background variations ,updated the credits given in the read me,and updated the list of functions i added in the read me

--- frogfrogfrog/README.md
+++ frogfrogfrog/README.md
@@ -10,11 +10,28 @@ A game about catching flies.
 
 Art:Jeany
 Sounds: 
+
 Title : https://pixabay.com/sound-effects/nature-sounds-splashing-water-birds-singing-frog-croak-brook-108620/
 
 In Game:https://pixabay.com/music/video-games-sinnesl%c3%b6schen-beam-117362/
 
+
  Gulp:https://pixabay.com/sound-effects/gulp-48452/
 
-https://p5js.org/examples/repetition-color-interpolation/
-https://github.com/Lartu/p5.clickable?tab=readme-ov-file
+
+
+
+
+ Shout out and credits to :
+( PERLIN NOISE )https://thecodingtrain.com/tracks/noise/noc/perlin/intro-to-perlin-noise (Very great youtuber when it comes to learning new function on js p5)
+(Title gradiant)https://p5js.org/examples/repetition-color-interpolation/
+(hmmmm i forgot)https://github.com/Lartu/p5.clickable?tab=readme-ov-file
+
+
+
+
+-Added title screen with instruction
+-Added button title screen
+-Added score which impact background n images
+-Constrain the frog 
+-Brand new dances move for the fly (BEAUTIFUL FLY MOVEMENTS WOW MY BABY IS A DANCER)
--- frogfrogfrog/js/script.js
+++ frogfrogfrog/js/script.js
@@ -15,18 +15,26 @@
 
 "use strict";
 //score system
-let score = 0; // Will count negative eggs from fly mamas eaten by frog
+let score = 0; 
 
+//background and fading
+let bgColor; 
+let targetColor; 
+
+//Perlin noise (future jeany remember to set them far apart so they arent the same)
+let noiseOffsetX = 0;
+let noiseOffsetY = 1000;
 
 //game states for navigation will start on title screen
 let gameState = "menu";
+
 //let images of spidey BE
 let spideytop;
 let spideybottom;
 let spideyleft;
 let spideyright;
 
-// Load the image.
+// Load all the images.
 function preload() {
     spideytop = loadImage('./assets/images/SpiderTop.png');
     spideyleft = loadImage('./assets/images/SpiderLeft.png');
@@ -67,7 +75,8 @@ function setup() {
     createCanvas(640, 480);
     // Give the fly its first random position
     resetFly();
-
+    bgColor = color("#87ceeb"); // light sky blue start
+    targetColor = bgColor;
 }
 function draw() { //where the gamestate come alive
     if (gameState === "menu") { //will be at title scren
@@ -85,6 +94,7 @@ function draw() { //where the gamestate come alive
 //a gradiant title screen going from ligth green to dark green+Click here to start function 
 function drawmenu() {
     rectMode();
+
     colorMode(HSB);
     noStroke();
     // Top color Hue: 100°, Saturation: 90%, Brightness: 100%
@@ -122,8 +132,44 @@ function drawmenu() {
 
 }
 
-function runGame() { //will let the game  start once game state is different 
-    background("#87ceeb");
+function runGame() { //will let the game  start once game state is different
+
+    //thank you geek for geeks and w3school and reddit .reddit the most love you nerds
+    //background will change with score
+    if (score > -100) {
+        targetColor = color("#aef2ff"); // very light blue (OH HAPPY DAYYYYYY)
+    }
+    else if (score <= -100 && score > -300) {
+        targetColor = color("#9ee7d5"); // mint green(HUH)
+    }
+    else if (score <= -300 && score > -600) {
+        targetColor = color("#b5f68d"); // light green(HMMM)
+    }
+    else if (score <= -600 && score > -900) {
+        targetColor = color("#f6e48d"); // yellow(WAIT...)
+    }
+    else if (score <= -900 && score > -1300) {
+        targetColor = color("#f6b68d"); // orange(OH LORD)
+    }
+    else if (score <= -1300 && score > -1700) {
+        targetColor = color("#f68d8d"); // redish pink(WAT)
+    }
+    else if (score <= -1700 && score > -2300) {
+        targetColor = color("#c34b4b"); // darker red (AH SXIT)
+    }
+    else if (score <= -2300 && score > -3000) {
+        targetColor = color("#632a2a"); // deep brown-red (WAIT WAIT SPIDEY)
+    }
+    else {
+        targetColor = color("#0f0f0f"); // near black (DEAD?)
+    }
+    //  fade between colors
+    bgColor = lerpColor(bgColor, targetColor, 0.02);
+    // draw background
+    background(bgColor);
+
+
+//regular fonction (organizing myself )
     moveFly();
     drawFly();
     moveFrog();
@@ -148,7 +194,7 @@ function runGame() { //will let the game  start once game state is different
         image(spideybottom, -5, 0, 600, 600);
     }
 
-    //Write score on corner left screen with text 
+    //Write score on corner left screen
     fill("black");
     textAlign(CENTER);
     textSize(25);
@@ -167,14 +213,20 @@ function runGame() { //will let the game  start once game state is different
 
 /* Moves the fly according to its speed  Resets the fly if it gets all the way to the right*/
 function moveFly() {
-    // Move the fly
-    fly.x += fly.speed;
-    // Handle the fly going off the canvas
-    if (fly.x > width) {
+    // Perlin noise movement
+    fly.x = noise(noiseOffsetX) * width;
+    fly.y = noise(noiseOffsetY) * 300; // vertical range for motion
+
+    // Increment noise which impact motion
+    noiseOffsetX += 0.01;
+    noiseOffsetY += 0.01;
+
+ // Handle the fly going off the canvas
+    if (fly.x > width - 10) {
         resetFly();
     }
-    //create one for fly going down and not vertically
 }
+
 
 // Draws the fly as a black circle
 
@@ -188,8 +240,12 @@ function drawFly() {
 
 //Resets the fly to the left with a random y
 function resetFly() {
-    fly.x = 0;
-    fly.y = random(0, 300);
+    // Randomize start so fly paths different
+    noiseOffsetX = random(0, 1000);
+    noiseOffsetY = random(1000, 2000);
+
+    // Optional: adjust fly speed or size to vary behavior a bit
+    fly.size = random(8, 14);
 }
 
 //Moves the frog with keyboard input + constraint body so not off screen
